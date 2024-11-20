d526ecf881b8faac668c4e088d19368f2a371810
TPNxl <tnadolsk@Purdue.edu>
2024-11-20 14:41:10 -0500

    Bug fixes to message_generator

--- convo_dataset.py
+++ convo_dataset.py
@@ -3,7 +3,15 @@ from text_dataset import *
 import torch
 import numpy as np
 import os
+from transformers import pipeline
+from huggingface_hub import login
+from openai import OpenAI
+from datetime import datetime
 
+with open("./tokens/hf_token.txt", 'r') as f:
+    hf_token = f.read().strip()
+with open("./tokens/openai_token.txt", 'r') as f:
+    openai_token = f.read().strip()
 with open("./prompts/first_prompt.txt", 'r') as f:
     FIRST_PROMPT = f.read().replace("\n", " ")
 with open("./prompts/continue_prompt.txt", 'r') as f:
@@ -14,11 +22,13 @@ with open("./prompts/eval_prompt.txt", 'r') as f:
     EVAL_PROMPT = f.read().replace("\n", " ")
 with open("./prompts/quality_buf.txt", 'r') as f:
     QUALITY_BUF = f.readlines()
-    QUALITY_BUF_LEN = len(QUALITY_BUF)
+    QUALITY_LEN = len(QUALITY_BUF)
 
 class Convo_Dataset(torch.utils.data.Dataset):
     def __init__(self, item_path="./convos/", llama_generate_func=None, gpt_generate_func=None, topics_path="./prompts/topics.txt", seed=12345):
         self.item_path = item_path
+        if not os.path.exists(item_path):
+            os.mkdir(item_path)
         self.llama_generate_func = llama_generate_func
         self.gpt_generate_func = gpt_generate_func
         self.topics_dataset = TextDataset(topics_path)
@@ -33,7 +43,9 @@ class Convo_Dataset(torch.utils.data.Dataset):
             item = self.topics_dataset[self.r.randint(0, self.TOPICS_LEN)]
             topic = item[0]
             v = item[1]
-            m = MessageGenerator(topic = topic)
+            m = MessageGenerator()
+            m.reset()
+            m.topic = topic
             random_letters = self.r.choice(alphabet, 2, replace=False)
             m.new_agent(random_letters[0], v[0], self.llama_generate_func)
             m.new_agent(random_letters[1], v[1], self.llama_generate_func)
@@ -45,15 +57,22 @@ class Convo_Dataset(torch.utils.data.Dataset):
                 evaluation = f"Who was more {quality}?"
 
             m.start(FIRST_PROMPT)
-            m.generate_debate(CONTINUE_PROMPT, debate_len)
+            m.generate_debate(CONTINUE_PROMPT, s_prompt_2=None, debate_len=debate_len)
             m.generate_ranking(EVAL_PROMPT, self.llama_generate_func, quality, evaluation)
             m.generate_feedback(FEEDBACK_PROMPT, self.gpt_generate_func, quality, evaluation)
             while True:
-                index = self.r.randint(0, np.power(2,16))
-                path = os.path.join(self.item_path, f"{header_name}{index}.hlst")
+                now = datetime.now()
+                formatted_string = now.strftime("%m%d%H%M")
+                index = self.r.randint(0, np.power(2,8)+100)
+                path = os.path.join(self.item_path, f"{header_name}{formatted_string}{index}.hlst")
                 if not os.path.exists(path):
                     m.save(path)
                     break
+            
+            del m
+            del random_letters
+            del v
+            del topic
 
     def load(self):
         for file in os.listdir(self.item_path):
@@ -68,4 +87,33 @@ class Convo_Dataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         return self.convos[idx]
         
-        
+if __name__ == "__main__":
+    MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
+    pipe = pipeline(
+        "text-generation",
+        model=MODEL_ID,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+
+    def llama_generate(messages: dict):
+        outputs = pipe(
+            messages,
+            max_new_tokens=256,
+            pad_token_id=None,
+        )
+        return outputs[0]["generated_text"][-1]
+
+    client = OpenAI(api_key=openai_token)
+
+    def gpt_generate(messages: dict):
+        chat_completion = client.chat.completions.create(
+            messages=messages,
+            model="gpt-4o-mini",
+        )
+        out = chat_completion.choices[0].message.content
+        out = out.split(": ")[-1]
+        return out
+    
+    c = Convo_Dataset(llama_generate_func=llama_generate, gpt_generate_func=gpt_generate)
+    c.generate(5)
--- message_generator.py
+++ message_generator.py
@@ -22,11 +22,24 @@ class MessageGenerator():
             self.history = []
             self.quality = quality
             self.evaluation = evaluation
+            self.rating = ""
             self.winner = ""
             self.loser = ""
             self.feedback = ""
-            self.last_agent = ""
-            self.same_agent_count = 1
+            self.curr_agent = 0
+    
+    def reset(self):
+        self.topic = ""
+        self.agents = []
+        self.agent_views = {}
+        self.agent_funcs = {}
+        self.history = []
+        self.quality = ""
+        self.evaluation = ""
+        self.rating = ""
+        self.winner = ""
+        self.loser = ""
+        self.feedback = ""
 
     def new_agent(self, agent_name, agent_view, agent_func):
         self.agents.append(agent_name)
@@ -43,6 +56,10 @@ class MessageGenerator():
         format_str = format_str.replace("<quality>", self.quality).replace("<evaluation>", self.evaluation)
         format_str = format_str.replace("<topic>", self.topic)
         format_str = format_str.replace("<winner>", self.winner).replace("<loser>", self.loser)
+        if agent_name is not None and agent_name != "":
+            format_str = format_str.replace("<agent_view>", self.agent_views[agent_name])
+        if opp_name is not None and opp_name != "":
+            format_str = format_str.replace("<opp_view>", self.agent_views[opp_name])
         return format_str
 
     def generate_starting_view(self, system_prompt, agent_name, opp_name, agent_view = None):
@@ -51,13 +68,12 @@ class MessageGenerator():
         if agent_view is None:
             self.agent_views[agent_name] = agent_view
         messages.append({"role": "system", "content": self.format_string(system_prompt, agent_name, opp_name)})
-        messages.append({"role": "system", "content": f"Please provide your initial view on the topic in one sentence, which is that {agent_view}."})
         return messages
     
     def start(self, system_prompt):
         assert len(self.agents) == 2
-        talking_order = r.permutation(range(0,2))
-        for i in talking_order:
+        self.curr_agent = r.randint(0, len(self.agents))
+        for i in range(0, len(self.agents)):
             agent = self.agents[i]
             msgs = self.generate_starting_view(system_prompt, agent, self.agents[(i+1)%2], self.agent_views[agent])
             self.append(agent, self.agent_funcs[agent](msgs))
@@ -68,7 +84,7 @@ class MessageGenerator():
         for h in self.history:
             role = "user" if agent_name is None or not h[0] == agent_name else "assistant"
             role = "system" if h[0] == "Judge" or h[0] == "Feedback" else role
-            messages.append({"role": role, "content": f"{h[0]}: {h[1]}"})
+            messages.append({"role": role, "content": f"{h[0]}: \"{h[1]['content']}\""})
         return messages
 
     def generate_debate_prompt(self, 
@@ -91,15 +107,16 @@ class MessageGenerator():
                         debate_len = 10,
                         ):
         for n in range(debate_len):
-            i = r.binomial(1, np.power(0.5, self.same_agent_count))
-            agent_name = self.agents[i]
-            if agent_name == self.last_agent:
-                self.same_agent_count += 1
-            else:
-                self.same_agent_count = 1
-            opp_name = self.agents[(i+1)%2]
+            agent_name = self.agents[self.curr_agent]
+            opp_name = self.agents[(self.curr_agent+1)%len(self.agents)]
             msgs = self.generate_debate_prompt(s_prompt_1, agent_name, opp_name, s_prompt_2)
-            self.append(agent_name, self.agent_funcs[agent_name](msgs))
+            agent_output = self.agent_funcs[agent_name](msgs)
+            agent_str = agent_output['content']
+            agent_str = agent_str.replace(f"{agent_name}: ", "")
+            agent_str = agent_str.replace("\"", "")
+            agent_output['content'] = agent_str
+            self.append(agent_name, agent_output)
+            self.curr_agent = (self.curr_agent + 1) % len(self.agents)
     
     def generate_ranking_prompt(self, ranking_prompt, quality = None, evaluation = None):
         messages = []
@@ -110,22 +127,25 @@ class MessageGenerator():
         ranking_prompt = self.format_string(ranking_prompt)
         messages.append({"role": "system", "content": ranking_prompt})
         messages.extend(self.generate_debate_history(agent_name = None))
-        messages.append({"role": "system", "content": "Your analysis starts now. Please end your analysis with the name of the winner on a line by itself."})
+        messages.append({"role": "system", "content": "Your analysis starts now. Please end your analysis with the line: \"Winner: Agent <name>\""})
         return messages
     
     def set_winner_from_prompt(self, prompt):
-        lines = prompt.split("\n")
-        for l in lines:
-            if l in self.agents:
-                self.winner = l
-                self.loser = (self.agents[1] if self.agents[0] == self.winner else self.agents[0]) if self.winner != "" else ""
-                return True
+        winner_line = prompt.split("\n")[-1]
+        try:
+            self.winner = winner_line.split("Winner: Agent ")[1]
+            self.loser = (self.agents[1] if self.agents[0] == self.winner else self.agents[0]) if self.winner != "" else ""
+            return True
+        except Exception:
+            print("Error setting winner from prompt")
+            print("Winner line:", winner_line)
         return False
     
     def generate_ranking(self, ranking_prompt, judge_func, quality = None, evaluation = None):
         msgs = self.generate_ranking_prompt(ranking_prompt, quality, evaluation)
         out = judge_func(msgs)
-        self.set_winner_from_prompt(out)
+        out['role'] = "system"
+        self.set_winner_from_prompt(out['content'])
         self.append("Judge", out)
     
     def generate_feedback_prompt(self, feedback_prompt, quality = None, evaluation = None):
@@ -144,8 +164,7 @@ class MessageGenerator():
     def generate_feedback(self, feedback_prompt, judge_func, quality = None, evaluation = None):
         msgs = self.generate_feedback_prompt(feedback_prompt, quality, evaluation)
         out = judge_func(msgs)
-        self.set_winner_from_prompt(out)
-        self.append("Feedback", out)
+        self.append("Feedback", {"role": "system", "content": out})
     
     def get(self, name):
         for h in self.history:
@@ -161,3 +180,23 @@ class MessageGenerator():
     def load(self, filename):
         with open(filename, 'rb') as f:
             self.topic, self.agents, self.agent_views, self.history, self.quality, self.evaluation, self.winner, self.loser, self.feedback = pickle.load(f)
+
+if __name__ == "__main__":
+    """
+    A client to open message files and print their contents in a nice manner with labels.
+    """
+    print("Welcome to the MessageGenerator client!")
+    fn = input("Please enter the filename of the message file you would like to load: ")
+    m = MessageGenerator(filename = fn)
+    print("Topic:", m.topic)
+    print("Agents:", m.agents)
+    print("Agent Views:", m.agent_views)
+    print("Quality:", m.quality)
+    print("Evaluation:", m.evaluation)
+    print("History:")
+    for h in m.history:
+        print(h)
+    print("Winner:", m.winner)
+    print("Loser:", m.loser)
+    print("Feedback:", m.feedback)
+   
